86f94920610395ce034e582956ee7051194a2d1d
Sarunas Gliebus <ssharunas@yahoo.co.uk>
2011-01-20 09:47:50 +0200

    implemented something

--- Repleisiukas/mainwindow.cpp
+++ Repleisiukas/mainwindow.cpp
@@ -6,12 +6,17 @@
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QSettings>
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+        QMainWindow(parent),
+        ui(new Ui::MainWindow)
 {
-	I_MENU_ITEMS = 10;
+    QCoreApplication::setOrganizationName("SoftDent");
+    QCoreApplication::setOrganizationDomain("softdent.lt");
+    QCoreApplication::setApplicationName("Repleisiukas");
+
+    I_MENU_ITEMS = 10;
     ui->setupUi(this);
     highlighter = new JSHighlighter(ui->query->document());
 
@@ -22,7 +27,7 @@ MainWindow::MainWindow(QWidget *parent) :
     strem = new QTextStream(last);
 
     ui->query->setPlainText(strem->readAll());
-	UpdateLastUsedMenu();
+    UpdateLastUsedMenu();
 }
 
 MainWindow::~MainWindow()
@@ -35,86 +40,82 @@ MainWindow::~MainWindow()
 }
 
 void MainWindow::UpdateLastUsedMenu(){
-	QStringList items;
-	QSettings settings;
+    QStringList items;
+    QSettings settings;
 
-	for(unsigned int i = 0; i < I_MENU_ITEMS; i++){
-		QString item = settings.value(QString("/files/load/%1").arg(i)).toString();
+    for(unsigned int i = 0; i < I_MENU_ITEMS; i++){
+        QString item = settings.value(QString("/files/load/%1").arg(i)).toString();
 
-		if(item.isEmpty())
-			break;
+        if(item.isEmpty())
+            break;
 
-		items.append(item);
-	}
+        items.append(item);
+    }
 
-	foreach (QString item, items) {
-		int index = item.lastIndexOf('/');
+    foreach (QString item, items) {
+        int index = item.lastIndexOf('/');
 
-		if(index == -1)
-			index = item.lastIndexOf('\\');
+        if(index == -1)
+            index = item.lastIndexOf('\\');
 
-		QString fileName = item;
+        QString fileName = item;
 
-		if(index != -1)
-			fileName = QString("%1 [%2]").arg(item.left(index)).arg(item);
+        if(index != -1)
+            fileName = QString("%1 [%2]").arg(item.left(index)).arg(item);
 
-		ui->menuLastUsed->addAction(fileName, this, SLOT(on_openFile_clicked()));
-	}
-	//ui->menuLastUsed
+        ui->menuLastUsed->addAction(fileName, this, SLOT(on_openFile_clicked()));
+    }
 }
 
-void MainWindow::UpdateLastUsedOrder(){
-	QSettings settings;
+void MainWindow::UpdateLastUsedOrder(QString newName){
+    QSettings settings;
 
-	for(int i = I_MENU_ITEMS; i > 1; i++){
-		QString prevItem = QString("/files/load/%1").arg(i-1);
-		QString item = QString("/files/load/%1").arg(i);
-		settings.setValue(item, settings.value(prevItem));
-	}
+    for(int i = I_MENU_ITEMS; i > 1; i++){
+        QString prevItem = QString("/files/load/%1").arg(i-1);
+        QString item = QString("/files/load/%1").arg(i);
+        settings.setValue(item, settings.value(prevItem));
+    }
 
-	QString item = QString("/files/load/%1").arg(0);
+    QString item = QString("/files/load/%1").arg(0);
 
-	settings.setValue(item, "lastUsed");
+    settings.setValue(item, newName);
 
-	UpdateLastUsedMenu();
+    UpdateLastUsedMenu();
 }
 
 void MainWindow::LoadFromFile(QString fileName){
-	if(!fileName.isEmpty()){
-		QFile file(fileName);
-		if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
-			QMessageBox::warning(this, "Could not open file", "could not open file");
-			return;
-		}
-		QTextStream stream(&file);
-		ui->query->setPlainText(stream.readAll());
-		UpdateLastUsedOrder();
-	}
+    if(!fileName.isEmpty()){
+        QFile file(fileName);
+        if (!file.open(QIODevice::ReadOnly | QIODevice::Text)){
+            QMessageBox::warning(this, "Could not open file", "could not open file");
+            return;
+        }
+        QTextStream stream(&file);
+        ui->query->setPlainText(stream.readAll());
+        UpdateLastUsedOrder(fileName);
+    }
 }
 
 void MainWindow::on_openFile_clicked(){
-	QAction *senderAction = dynamic_cast<QAction*>(sender());
+    QAction *senderAction = dynamic_cast<QAction*>(sender());
 
-	if(senderAction){
-		QString file = senderAction->data().toString();
-		LoadFromFile(file);
-	}
+    if(senderAction){
+        QString file = senderAction->data().toString();
+        LoadFromFile(file);
+    }
 }
 
 void MainWindow::on_pushButton_Go_clicked()
 {
-    //strem->device()->reset();
     last->resize(0);
 
     *strem << ui->query->toPlainText();
     strem->flush();
 
     QScriptEngine engine;
-	QString in = ui->stringIn->text();
-//    in = in.replace('\'', "\\'");
-//    in = in.replace('\n', "\\n");
+    QString in = ui->stringIn->text();
 
-	QString query =  QString("%1 ; \n%2").arg(in).arg(ui->query->toPlainText());
+    QString query =  QString("%1 ; \n%2").arg(in).arg(ui->query->toPlainText());
     qDebug() << query;
     QScriptValue value = engine.evaluate(query);
 
@@ -138,8 +139,20 @@ void MainWindow::on_actionSave_triggered()
 
 void MainWindow::on_actionLoad_triggered()
 {
-    QString fileName = QFileDialog::getOpenFileName(this);
-	LoadFromFile(fileName);
+    QSettings settings;
+    QString path =  settings.value("/files/load/0").toString();
+    int index = path.lastIndexOf("/");
+    if(index == -1)
+        index = path.lastIndexOf("\\");
+    if(index == -1)
+        index = path.length();
+
+    path = path.left(index);
+
+    qDebug() << path;
+
+    QString fileName = QFileDialog::getOpenFileName(this, QString(), path);
+    LoadFromFile(fileName);
 }
 
 void MainWindow::on_pushButton_clicked()
--- Repleisiukas/mainwindow.h
+++ Repleisiukas/mainwindow.h
@@ -23,14 +23,15 @@ private:
     QTextStream* strem;
     QFile *last;
     JSHighlighter *highlighter;
-	unsigned int I_MENU_ITEMS;
+    unsigned int I_MENU_ITEMS;
 
 private:
-	void UpdateLastUsedMenu();
-	void UpdateLastUsedOrder();
-	void LoadFromFile(QString file);
+    void UpdateLastUsedMenu();
+    void UpdateLastUsedOrder(QString newName);
+    void LoadFromFile(QString file);
+
 private slots:
-	void on_openFile_clicked();
+    void on_openFile_clicked();
     void on_pushButton_clicked();
     void on_actionLoad_triggered();
     void on_actionSave_triggered();
